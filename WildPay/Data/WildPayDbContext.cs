fa03d6fcdea25b55ae11bf36d701d944325da958
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-07-24 16:34:50 +0200

    Installed the correct Entity Design package

---
+++ WildPay/Data/WildPayDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WildPay.Models.Entities;
+
+namespace WildPay.Data
+{
+    public class WildPayDbContext : DbContext
+    {
+        public WildPayDbContext(DbContextOptions<WildPayDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Expenditure> Expenditures { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- WildPay/Program.cs
+++ WildPay/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WildPay.Data;
+
 namespace WildPay
 {
     public class Program
@@ -8,6 +11,11 @@ namespace WildPay
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<WildPayDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
