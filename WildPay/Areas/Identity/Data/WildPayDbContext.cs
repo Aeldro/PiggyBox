569e6cd49a89f9266188fb0e20f021b643e5b2ef
popobg <pauline.bouyssou1@gmail.com>
2024-07-25 20:38:08 +0200

    Add FK GroupId to Expenditure and configure FK behavior + Add data before migration.

--- WildPay/Areas/Identity/Data/WildPayDbContext.cs
+++ WildPay/Areas/Identity/Data/WildPayDbContext.cs
@@ -20,9 +20,166 @@ public class WildPayDbContext : IdentityDbContext<ApplicationUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
 
+        // if the group is delete,
+        // all the categories related to this group will be deleted.
+        builder.Entity<Category>()
+            .HasOne(c => c.Group)
+            .WithMany(g => g.Categories)
+            .HasForeignKey(c => c.GroupId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Cascade doesn't work so we will have to manage in our code
+        // the deletion of all the expenditures before the delete of the group
+        builder.Entity<Expenditure>()
+            .HasOne(e => e.Group)
+            .WithMany(g => g.Expenditures)
+            .HasForeignKey(e => e.GroupId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // if a user is delete,
+        // the expenditure will not be deleted.
+        // ApplicationUserId will only be set to the default value (empty string).
+        builder.Entity<Expenditure>()
+            .HasOne(e => e.ApplicationUser)
+            .WithMany(g => g.Expenditures)
+            .HasForeignKey(e => e.ApplicationUserId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        // same, but it is CategoryId that will be set to null.
+        builder.Entity<Expenditure>()
+            .HasOne(e => e.Category)
+            .WithMany(g => g.Expenditures)
+            .HasForeignKey(e => e.CategoryId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        var hasher = new PasswordHasher<IdentityUser>();
+
+        builder.Entity<ApplicationUser>().HasData(
+            new ApplicationUser
+            {
+                Id = "1",
+                Firstname = "Helena",
+                Lastname = "Yamasaki",
+                UserName = "Lele",
+                NormalizedUserName = "LELE",
+                Email = "helena@gmail.com",
+                NormalizedEmail = "HELENA@GMAIL.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Helena.123")
+            },
+            new ApplicationUser
+            {
+                Id = "2",
+                Firstname = "Pauline",
+                Lastname = "Bouyssou",
+                UserName = "popo",
+                NormalizedUserName = "POPO",
+                Email = "pauline@gmail.com",
+                NormalizedEmail = "PAULINE@GMAIL.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Pauline.123")
+            },
+            new ApplicationUser
+            {
+                Id = "3",
+                Firstname = "Nolan",
+                Lastname = "De Puydt",
+                UserName = "Nono",
+                NormalizedUserName = "NONO",
+                Email = "nolan@gmail.com",
+                NormalizedEmail = "NOLAN@GMAIL.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Nolan.123")
+            },
+            new ApplicationUser
+            {
+                Id = "4",
+                Firstname = "Kevin",
+                Lastname = "Osei Yaw",
+                UserName = "Kev",
+                NormalizedUserName = "KEV",
+                Email = "kevin@gmail.com",
+                NormalizedEmail = "KEVIN@GMAIL.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Kevin.123")
+            });
+
+        builder.Entity<Group>().HasData(
+            new Group()
+            {
+                Id = 1,
+                Name = "Coloc"
+            },
+            new Group()
+            {
+                Id = 2,
+                Name = "Vacances d'été"
+            },
+            new Group()
+            {
+                Id = 3,
+                Name = "Voyage scolaire"
+            });
+
+        builder.Entity<Category>().HasData(
+            new Category()
+            {
+                Id = 1,
+                Name = "Courses",
+                GroupId = 1
+            },
+            new Category()
+            {
+                Id = 2,
+                Name = "Plaisirs coupables",
+                GroupId = 1
+            },
+            new Category()
+            {
+                Id = 3,
+                Name = "Restaurants",
+                GroupId = 1
+            });
+
+        builder.Entity<Expenditure>().HasData(
+            new Expenditure()
+            {
+                Id = 1,
+                Name = "Courses Auchan",
+                Amount = 10.5,
+                Date = new DateTime(2024, 04, 17),
+                ApplicationUserId = "1",
+                GroupId = 1
+            },
+            new Expenditure()
+            {
+                Id = 2,
+                Name = "Restaurant Toulouse",
+                Amount = 60,
+                Date = new DateTime(2024, 05, 01),
+                ApplicationUserId = "1",
+                CategoryId = 3,
+                GroupId = 1
+            },
+            new Expenditure()
+            {
+                Id = 3,
+                Name = "Café Capitole",
+                Amount = 16.5,
+                Date = new DateTime(2024, 04, 29),
+                ApplicationUserId = "2",
+                GroupId = 2
+            },
+            new Expenditure()
+            {
+                Id = 4,
+                Name = "Bonbons",
+                Amount = 6.9,
+                Date = new DateTime(2024, 05, 10),
+                ApplicationUserId = "3",
+                CategoryId = 2,
+                GroupId = 1
+            });
     }
 }
--- WildPay/Models/Entities/Expenditure.cs
+++ WildPay/Models/Entities/Expenditure.cs
@@ -1,14 +1,20 @@
-﻿namespace WildPay.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WildPay.Models.Entities
 {
     public class Expenditure
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
         public double Amount { get; set; }
         public DateTime Date { get; set; }
-        public string ApplicationUserId { get; set; } = string.Empty;
+        public string? ApplicationUserId { get; set; } = null;
         public ApplicationUser? ApplicationUser { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         public Category? Category { get; set; }
+        public int? GroupId { get; set; }
+        public Group? Group { get; set; }
     }
 }
