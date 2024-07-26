020e45e0ac0703c2937aa2231202a40151fcb2e1
Helena Miura <helenamiura@gmail.com>
2024-07-26 17:04:26 +0200

    added GroupController.cs

--- WildPay/Controllers/GroupController.cs
+++ WildPay/Controllers/GroupController.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using WildPay.Models.Entities;
 
 namespace WildPay.Controllers;
 
 public class GroupController : Controller
 {
+    
     // GET
-    public IActionResult Index()
+    [HttpGet]
+    public IActionResult List()
     {
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Group group)
+    {
+        return RedirectToAction(actionName: "List", controllerName: "Group");
     }
 }
