d831d2a3cb5220fe0ba94e7941293ba2a0e2d186
popobg <pauline.bouyssou1@gmail.com>
2024-07-29 17:42:14 +0200

    Add ICategoryRepository and CategoryRepository

--- WildPay/Interfaces/ICategoryRepository.cs
+++ WildPay/Interfaces/ICategoryRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace WildPay.Interfaces
+﻿using WildPay.Models.Entities;
+
+namespace WildPay.Interfaces
 {
     public interface ICategoryRepository
     {
+        Task<List<Category>> GetCategoriesAsync(int groupId);
+        Task<Category?> GetCategoryByIdAsync(int categoryId);
+        Task EditCategoryAsync(Category category);
+        Task AddCategoryAsync(string name, int groupId);
+        Task<bool> DeleteCategoryAsync(int categoryId);
     }
 }
---
+++ WildPay/Repositories/CategoryRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WildPay.Data;
+using WildPay.Interfaces;
+using WildPay.Models.Entities;
+
+namespace WildPay.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly WildPayDbContext _context;
+
+        public CategoryRepository(WildPayDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync(int groupId)
+        {
+            List<Category> categories = await _context.Categories
+                .Where(c => c.GroupId == groupId)
+                .Select(category => category)
+                .ToListAsync();
+
+            return categories;
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int categoryId)
+        {
+            Category? category = await _context.Categories.FindAsync(categoryId);
+            return category;
+        }
+
+        public async Task EditCategoryAsync(Category category)
+        {
+            if (await _context.Categories.FindAsync(category.Id) is Category found && found != null)
+            {
+                found.Name = category.Name;
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task AddCategoryAsync(string name, int groupId)
+        {
+            Category newCategory = new Category
+            {
+                Name = name,
+                GroupId = groupId
+            };
+
+            await _context.Categories.AddAsync(newCategory);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteCategoryAsync(int categoryId)
+        {
+            if (await _context.Categories.FindAsync(categoryId) is Category found)
+            {
+                _context.Categories.Remove(found);
+
+                // This method is override in WildPayDbContext
+                // and delete all the expenditures linked to the removed group
+                // before deleting the group
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
