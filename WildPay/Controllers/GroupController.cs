bd6743cd56eabbf1f46aee10ca7f847751494d69
Helena Miura <helenamiura@gmail.com>
2024-07-29 15:56:39 +0200

    added methods CRUD to GroupController.cs

--- WildPay/Controllers/GroupController.cs
+++ WildPay/Controllers/GroupController.cs
@@ -1,27 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
+using WildPay.Interfaces;
 using WildPay.Models.Entities;
 
 namespace WildPay.Controllers;
 
 public class GroupController : Controller
 {
-    
-    // GET
-    [HttpGet]
-    public IActionResult List()
+    private readonly IGroupRepository _repository;
+    public GroupController(IGroupRepository repository)
     {
-        return View();
+        _repository = repository;
     }
 
+    // READ
+    [HttpGet]
+    public async Task<IActionResult> List()
+    {
+        var groups = await _repository.GetGroupsAsync();
+        return View(groups);
+    }
+
+    // CREATE view
     [HttpGet]
     public IActionResult Create()
     {
         return View();
     }
 
+    // CREATE action
     [HttpPost]
-    public IActionResult Create(Group group)
+    public async Task<IActionResult> Create(Group group)
     {
+        await _repository.AddGroupAsync(group.Name, group.Image);
+        return RedirectToAction(actionName: "List", controllerName: "Group");
+    }
+    
+    // UPDATE view
+    [HttpGet]
+    public IActionResult Edit()
+    {
+        return View();
+    }
+    
+    // UPDATE action
+    [HttpPost]
+    public async Task<IActionResult> Update(Group group)
+    {
+        await _repository.EditGroupAsync(group);
+        return RedirectToAction(actionName: "List", controllerName: "Group");
+    }
+
+    // DELETE view 
+    [HttpGet]
+    public IActionResult Delete()
+    {
+        return View();
+    }
+    
+    // DELETE action 
+    [HttpPost]
+    public async Task<IActionResult> Delete(int groupId)
+    {
+        await _repository.DeleteGroupAsync(groupId);
         return RedirectToAction(actionName: "List", controllerName: "Group");
     }
 }
