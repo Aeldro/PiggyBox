28905015bf70cbe46476117133f9f1fbd28bc7f2
popobg <pauline.bouyssou1@gmail.com>
2024-07-29 15:11:52 +0200

    Add repositories and interface and implement GroupRepository

---
+++ WildPay/Interfaces/IGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using WildPay.Models.Entities;
+
+namespace WildPay.Interfaces
+{
+    public interface IGroupRepository
+    {
+        Task<List<Group>> GetGroupsAsync();
+        Task<Group?> GetGroupByIdAsync(int groupId);
+        Task EditGroupAsync(Group group);
+        Task AddGroupAsync(string name, string Image);
+        Task<bool> DeleteGroupAsync(int groupId);
+    }
+}
---
+++ WildPay/Repositories/GroupRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WildPay.Data;
+using WildPay.Interfaces;
+using WildPay.Models.Entities;
+
+namespace WildPay.Repositories
+{
+    public class GroupRepository : IGroupRepository
+    {
+        private readonly WildPayDbContext _context;
+
+        public GroupRepository(WildPayDbContext context)
+        {
+            _context = context;
+        }
+
+        private WildPayDbContext Get_context()
+        {
+            return _context;
+        }
+
+        public async Task<List<Group>> GetGroupsAsync()
+        {
+            List<Group> groups = await _context.Groups.ToListAsync();
+            return groups;
+        }
+
+        public async Task<Group?> GetGroupByIdAsync(int groupId)
+        {
+            Group? group = await _context.Groups.FindAsync(groupId);
+            return group;
+        }
+
+        public async Task EditGroupAsync(Group group)
+        {
+            if (await _context.Groups.FindAsync(group.Id) is Group found && found != null)
+            {
+                found.Name = group.Name;
+                found.Image = group.Image;
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task AddGroupAsync(string name, string image)
+        {
+            Group newGroup = new Group()
+            {
+                Name = name,
+                Image = image
+            };
+
+            await _context.Groups.AddAsync(newGroup);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteGroupAsync(int groupId)
+        {
+            if (await _context.Groups.FindAsync(groupId) is Group found)
+            {
+                _context.Groups.Remove(found);
+
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
