e30ad00d3381de62f2d4266aef610fe3d35ed49b
Helena Miura <helenamiura@gmail.com>
2024-07-29 15:12:02 +0200

    added methods CRUD to ExpenditureController.cs

--- WildPay/Controllers/ExpenditureController.cs
+++ WildPay/Controllers/ExpenditureController.cs
@@ -5,20 +5,51 @@ namespace WildPay.Controllers;
 
 public class ExpenditureController : Controller
 {
-    // GET
+    
+    // READ
     public IActionResult List()
     {
         return View();
     }
     
+    // UPDATE
     [HttpGet]
     public IActionResult Edit()
     {
         return View();
     }
 
+    // UPDATE
     [HttpPost]
     public IActionResult Edit(Expenditure expenditure)
+    {
+        return RedirectToAction(actionName: "List", controllerName: "Expenditure");
+    }
+
+    // CREATE
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // CREATE
+    [HttpPost]
+    public IActionResult Create(Expenditure expenditure)
+    {
+        return RedirectToAction(actionName: "List", controllerName: "Expenditure");
+    }
+
+    // DELETE
+    [HttpGet]
+    public IActionResult Delete()
+    {
+        return View();
+    }
+
+    // DELETE
+    [HttpPost]
+    public IActionResult Delete(Expenditure expenditure)
     {
         return RedirectToAction(actionName: "List", controllerName: "Expenditure");
     }
