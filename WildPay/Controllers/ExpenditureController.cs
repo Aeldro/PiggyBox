f0f1456b9a9928ea79f286093ad47803dc9ea01c
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-08-02 17:30:47 +0200

    contributors.count = 0

--- WildPay/Controllers/ExpenditureController.cs
+++ WildPay/Controllers/ExpenditureController.cs
@@ -23,6 +23,20 @@ public class ExpenditureController : Controller
     }
 
     // READ
+    async public Task<IActionResult> ListGroupExpenditures(int Id)
+    {
+        //Get the group
+        Group? group = await _groupRepository.GetGroupByIdAsync(Id);
+
+        //Return not found if no group is found
+        if (group == null) { return NotFound(); }
+
+        //Verify if the User belongs to the group, else we block the access
+        if (_userManager.GetUserId(User) is null || group.ApplicationUsers.FirstOrDefault(el => el.Id == _userManager.GetUserId(User)) is null) { return NotFound(); }
+
+        return View(group);
+    }
+
     async public Task<IActionResult> ListGroupBalances(int Id)
     {
         //Get the group
@@ -39,7 +53,7 @@ public class ExpenditureController : Controller
         groupBalance.Group = group;
         groupBalance.TotalAmount = group.Expenditures.Sum(el => el.Amount);
 
-        Dictionary<ApplicationUser, double> membersBalance = CalculateMembersBalance(group); //Calculate the balance of each member
+        Dictionary<string, double> membersBalance = CalculateMembersBalance(group); //Calculate the balance of each member
         membersBalance.OrderBy(el => el.Value);
         groupBalance.UsersBalance = membersBalance;
         groupBalance = CalculateDebtsList(groupBalance, group); //Calculate who must pay who
@@ -51,36 +65,22 @@ public class ExpenditureController : Controller
         return View(groupBalance);
     }
 
-    async public Task<IActionResult> ListGroupExpenditures(int Id)
-    {
-        //Get the group
-        Group? group = await _groupRepository.GetGroupByIdAsync(Id);
-
-        //Return not found if no group is found
-        if (group == null) { return NotFound(); }
-
-        //Verify if the User belongs to the group, else we block the access
-        if (_userManager.GetUserId(User) is null || group.ApplicationUsers.FirstOrDefault(el => el.Id == _userManager.GetUserId(User)) is null) { return NotFound(); }
-
-        return View(group);
-    }
-
     //Used in the GroupBalances method to calculate the balance of each member
-    public Dictionary<ApplicationUser, double> CalculateMembersBalance(Group group)
+    public Dictionary<string, double> CalculateMembersBalance(Group group)
     {
-        Dictionary<ApplicationUser, double> membersBalance = new Dictionary<ApplicationUser, double>();
+        Dictionary<string, double> membersBalance = new Dictionary<string, double>();
 
-        foreach (ApplicationUser member in group.ApplicationUsers) membersBalance.Add(member, 0);
+        foreach (ApplicationUser member in group.ApplicationUsers) membersBalance.Add(member.Id, 0);
 
         foreach (Expenditure expenditure in group.Expenditures)
         {
             if (expenditure.PayerId is not null && expenditure.Payer is not null)
             {
                 double expenditureContributionPerPerson = expenditure.Amount / expenditure.RefundContributors.Count;
-                membersBalance[expenditure.Payer] += expenditure.Amount;
+                membersBalance[expenditure.PayerId] = membersBalance[expenditure.PayerId] + expenditure.Amount;
                 foreach (ApplicationUser contributor in expenditure.RefundContributors)
                 {
-                    membersBalance[contributor] -= expenditureContributionPerPerson;
+                    membersBalance[contributor.Id] = membersBalance[contributor.Id] - expenditureContributionPerPerson;
                 }
             }
         }
@@ -92,9 +92,9 @@ public class ExpenditureController : Controller
     public GroupBalance CalculateDebtsList(GroupBalance groupBalance, Group group)
     {
         //Split the users balances into two parts : positives balances and negative balances. This aims to make the further calculation easier
-        Dictionary<ApplicationUser, double> positiveBalanceMembers = new Dictionary<ApplicationUser, double>();
-        Dictionary<ApplicationUser, double> negativeBalanceMembers = new Dictionary<ApplicationUser, double>();
-        foreach (KeyValuePair<ApplicationUser, double> member in groupBalance.UsersBalance)
+        Dictionary<string, double> positiveBalanceMembers = new Dictionary<string, double>();
+        Dictionary<string, double> negativeBalanceMembers = new Dictionary<string, double>();
+        foreach (KeyValuePair<string, double> member in groupBalance.UsersBalance)
         {
             if (member.Value < 0) negativeBalanceMembers.Add(member.Key, member.Value);
             else if (member.Value > 0) positiveBalanceMembers.Add(member.Key, member.Value);
@@ -104,10 +104,10 @@ public class ExpenditureController : Controller
         while (positiveBalanceMembers.Any(el => el.Value > 0.01) && negativeBalanceMembers.Any(el => el.Value < 0.01))
         {
             //Match the member who has the highest balance to the member who has the lowest balance
-            KeyValuePair<ApplicationUser, double> positiveBalanceMember = positiveBalanceMembers.OrderByDescending(el => el.Value).First();
-            ApplicationUser positiveBalanceUser = group.ApplicationUsers.Find(el => el.Id == positiveBalanceMember.Key.Id);
-            KeyValuePair<ApplicationUser, double> negativeBalanceMember = negativeBalanceMembers.OrderBy(el => el.Value).First();
-            ApplicationUser negativeBalanceUser = group.ApplicationUsers.Find(el => el.Id == negativeBalanceMember.Key.Id);
+            KeyValuePair<string, double> positiveBalanceMember = positiveBalanceMembers.OrderByDescending(el => el.Value).First();
+            ApplicationUser positiveBalanceUser = group.ApplicationUsers.Find(el => el.Id == positiveBalanceMember.Key);
+            KeyValuePair<string, double> negativeBalanceMember = negativeBalanceMembers.OrderBy(el => el.Value).First();
+            ApplicationUser negativeBalanceUser = group.ApplicationUsers.Find(el => el.Id == negativeBalanceMember.Key);
 
             double amount = 0;
 
