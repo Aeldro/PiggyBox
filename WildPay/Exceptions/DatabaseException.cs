6af2be05e1dc51651474e5f8ed5c21b13a1b680b
Aeldro <nodepuydt@gmail.com>
2024-08-15 13:39:49 +0200

    Exceptions ok

---
+++ WildPay/Exceptions/DatabaseException.cs
@@ -0,0 +1,21 @@
+﻿namespace WildPay.Exceptions
+{
+
+    public class DatabaseException : Exception
+    {
+        public DatabaseException()
+            : base("La base de données est temporairement indisponible.")
+        {
+        }
+
+        public DatabaseException(string message)
+    : base(message)
+        {
+        }
+
+        public DatabaseException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
