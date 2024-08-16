ad8a05a3dc43383b737e0948f2a3ac895278c7a4
popobg <pauline.bouyssou1@gmail.com>
2024-08-16 12:05:12 +0200

    Implement a JS function to enable/disable the save button on the update profile form

---
+++ WildPay/wwwroot/js/isFormDirty.js
@@ -0,0 +1,66 @@
+﻿//import { IsTypeValid } from "./imagePreview.js";
+//import { IsSizeValid } from "./imagePreview.js";
+
+function setupChangeDetection(formId, buttonId) {
+    let form = document.getElementById(formId);
+    let button = document.getElementById(buttonId);
+    let initialValues = {};
+
+    // store initial values of the form fields
+    Array.from(form.elements).forEach(function (element) {
+        if (element.tagName.toLowerCase() !== 'button'
+            && element.type !== 'submit') {
+            initialValues[element.name] = element.value;
+        }
+    });
+
+    // inner function
+    function isFormDirty() {
+        for (var key in initialValues) {
+            if (initialValues.hasOwnProperty(key)) {
+                var element = form.elements[key];
+
+                if (element && element.value !== initialValues[key]) {
+                    if (element.id === "customFile") {
+                        file = document.getElementById(element.id).files[0];
+
+                        if (!IsImageValid(file)) {
+                            continue;
+                        }
+                    }
+
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // change event listener to each input field
+    Array.from(form.elements).forEach(function (element) {
+        if (element.tagName.toLowerCase() !== 'button'
+            && element.type !== 'submit') {
+            element.addEventListener('input', function () {
+                button.disabled = !isFormDirty();
+            });
+        }
+    });
+}
+
+function IsImageValid(file) {
+    const fileType = file.type.toLowerCase();
+    const validTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+
+    const fileSize = file.size;
+    const maxSize = 2 * 1024 * 1024;
+
+    if (!validTypes.includes(fileType)) {
+        return false;
+    }
+
+    if (fileSize > maxSize) {
+        return false;
+    }
+
+    return true;
+}
