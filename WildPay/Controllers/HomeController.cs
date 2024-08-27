2a383e618b9f50cde7fcf1e1e8e9d02c56fba6c1
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-08-27 10:49:02 +0200

    Home page ok and responsive

--- WildPay/Controllers/HomeController.cs
+++ WildPay/Controllers/HomeController.cs
@@ -1,21 +1,44 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WildPay.Exceptions;
 using WildPay.Models;
+using WildPay.Models.Entities;
+using WildPay.Repositories.Interfaces;
 
 namespace WildPay.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IGroupRepository _groupRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, IGroupRepository groupRepository)
         {
             _logger = logger;
+            _userManager = userManager;
+            _groupRepository = groupRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                ApplicationUser currentUser = await _userManager.GetUserAsync(User);
+
+                var userId = _userManager.GetUserId(User);
+
+                string firstname = currentUser is not null ? currentUser.Firstname : string.Empty;
+                ViewBag.Firstname = firstname;
+
+                var groups = await _groupRepository.GetGroupsAsync(userId);
+                return View(groups);
+            }
+            catch (DatabaseException ex)
+            {
+                return RedirectToAction(actionName: "Exception", controllerName: "Home", new { message = ex.Message });
+            }
         }
 
         public IActionResult Privacy()
